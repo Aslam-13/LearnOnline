bf7b28966cf3fca6238c243a182de122653dae04
syed aslam uddin <syedaslamuddin74@gmail.com>
2022-10-29 19:21:15 +0530

    db added

--- config/db.js
+++ config/db.js
@@ -2,8 +2,11 @@ const mongoose = require('mongoose');
 
 const connectDB = async ()=>{
   const conn = await mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false
-  })
-}
+    useNewUrlParser: true, 
+    useUnifiedTopology: true
+  });
+  console.log(`mongodb connected ${conn.connection.host}`);
+};
+
+
+module.exports = connectDB;
