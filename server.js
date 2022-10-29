9913973698a0aff71edd5e461a7af9301149cecc
syed aslam uddin <syedaslamuddin74@gmail.com>
2022-10-29 19:24:55 +0530

    added colors

--- server.js
+++ server.js
@@ -1,15 +1,25 @@
 const express = require('express');
 const dotenv = require('dotenv');
-
+const morgan = require('morgan');
+const connectDB = require('./config/db');
+const colors = require('colors');
 
 // route files
 const bootcamps = require('./routes/bootcamps');
 
 dotenv.config({path: './config/config.env'});
 
+
+// connect to db
+connectDB();
+
+
 const app = express();
 
-
+// login middleware
+if(process.env.NODE_ENV === 'development'){
+  app.use(morgan('dev'));
+}
 // Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
 
@@ -17,7 +27,15 @@ app.use('/api/v1/bootcamps', bootcamps);
 const PORT = process.env.PORT || 5000;
 
 
-app.listen(
+const server = app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold)
 );
+
+
+// handle unhandled promise rejecions
+process.on('unhandledRejection', (err, promise)=>{
+  console.log(`Error: ${err.message}`.red);
+  // close server & exit process
+  server.close(()=> process.exit(1));
+})
