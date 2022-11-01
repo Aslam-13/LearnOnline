2b92d6d5333e45af09fdf5a115e0bf773da4b94b
syed aslam uddin <syedaslamuddin74@gmail.com>
2022-11-01 13:16:16 +0530

    middlewares

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const dotenv = require('dotenv');
 const morgan = require('morgan');
 const connectDB = require('./config/db');
 const colors = require('colors');
+const errorHandler = require('./middleware/error')
 
 // route files
 const bootcamps = require('./routes/bootcamps');
@@ -16,12 +17,16 @@ connectDB();
 
 const app = express();
 
+app.use(express.json());
+
 // login middleware
 if(process.env.NODE_ENV === 'development'){
   app.use(morgan('dev'));
 }
 // Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
+
+app.use(errorHandler);
 
 
 const PORT = process.env.PORT || 5000;
