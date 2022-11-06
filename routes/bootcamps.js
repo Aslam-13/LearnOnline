82890ba03d1c793fe44b2feb9d41c4001d88ba1e
syed aslam uddin <syedaslamuddin74@gmail.com>
2022-11-06 10:07:56 +0530

    get with id and update with id courses

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -3,6 +3,11 @@ const router = express.Router();
 const { getBootcamps, getBootcamp, createBootcamp, updateBootcamp, deleteBootcamp} = require('../controllers/bootcamps')
  
 
+// other resouces routers
+const courseRouter = require('./courses');
+// Re-route into other resource
+router.use('/:bootcampId/courses', courseRouter);
+
 router.route('/').get(getBootcamps).post(createBootcamp);
 router.route('/:id').get(getBootcamp).put(updateBootcamp).delete(deleteBootcamp);
 
