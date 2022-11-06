647803439e69df014876c201cee0b97f36024546
syed aslam uddin <syedaslamuddin74@gmail.com>
2022-11-06 19:14:34 +0530

    jwt added

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const path = require('path');
 // route files
 const bootcamps = require('./routes/bootcamps');
 const courses = require('./routes/courses');
+const auth = require('./routes/auth');
 
 dotenv.config({path: './config/config.env'});
 
@@ -32,6 +33,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 // Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
 app.use('/api/v1/courses', courses);
+app.use('/api/v1/auth', auth);
 
 app.use(errorHandler);
 
