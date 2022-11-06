4154d792b92f4c6eede60c8cc8bddfaf847fad32
syed aslam uddin <syedaslamuddin74@gmail.com>
2022-11-06 19:13:53 +0530

    user controller

---
+++ controllers/auth.js
@@ -0,0 +1,21 @@
+const User = require('../models/User');
+const ErrorResponse = require('../utils/ErrorResponse');
+const asyncHandler = require('../middleware/async');
+
+
+exports.register = asyncHandler(async (req, res, next)=>{
+  const {name, email, password, role} = req.body;
+
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+    role
+  });
+
+  const token = user.getSignedJwtToken();
+
+
+  res.status(200).json({success: true, token});
+})
