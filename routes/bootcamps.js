02e8afa0613d4b2925534419be8a88479189008a
syed aslam uddin <syedaslamuddin74@gmail.com>
2022-11-08 16:34:34 +0530

    more routes added

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -3,14 +3,14 @@ const router = express.Router();
 const { getBootcamps, getBootcamp, createBootcamp, updateBootcamp, deleteBootcamp, bootcampPhotoUpload} = require('../controllers/bootcamps')
  const advancedResults  = require('../middleware/advancedResults');
  const Bootcamp = require('../models/Bootcamp');
-
+const {protect, authorize} = require('../middleware/auth');
 // other resouces routers
 const courseRouter = require('./courses');
 // Re-route into other resource
 router.use('/:bootcampId/courses', courseRouter);
 
-router.route('/').get(advancedResults(Bootcamp, 'courses'), getBootcamps).post(createBootcamp);
-router.route('/:id').get(getBootcamp).put(updateBootcamp).delete(deleteBootcamp);
-router.route('/:id/photo').put(bootcampPhotoUpload);
+router.route('/').get(advancedResults(Bootcamp, 'courses'), getBootcamps).post(protect, authorize('publisher', 'admin'), createBootcamp);
+router.route('/:id').get(getBootcamp).put(protect, authorize('publisher', 'admin'), updateBootcamp).delete(protect, authorize('publisher', 'admin'), deleteBootcamp);
+router.route('/:id/photo').put(protect, authorize('publisher', 'admin'), bootcampPhotoUpload);
   
 module.exports = router;
