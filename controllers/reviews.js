eded177571951738316a492916bc7a948a1ac05c
syed aslam uddin <syedaslamuddin74@gmail.com>
2022-11-27 12:16:42 +0530

    added update and delete revies

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -33,3 +33,47 @@ exports.getReview = asyncHandler(async (req, res, next)=>{
     data: review
   })
 })
+exports.addReview = asyncHandler(async (req, res, next)=>{
+   req.body.bootcamp = req.params.bootcampId;
+   req.body.user = req.user.id;
+   const bootcamp = await Bootcamp.findById(req.params.bootcampId);
+   if(!bootcamp){
+    return next(new ErrorResponse(`No bootcamp with the id of ${req.params.bootcampId}`, 404));
+   }
+   const review = await Review.create(req.body);
+  res.status(201).json({
+    success: true,
+    data: review
+  })
+})
+exports.updateReview = asyncHandler(async (req, res, next)=>{ 
+   let review = await Review.findById(req.params.id);
+   if(!review){
+    return next(new ErrorResponse(`No review with the id of ${req.params.id}`, 404));
+   } 
+   if(review.user.toString()!= req.user.id && req.user.role!=='admin'){
+    return next(new ErrorResponse('Not authorize to update review', 401))
+   }
+     review = await Review.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true
+   })
+  res.status(201).json({
+    success: true,
+    data: review
+  })
+})
+exports.deleteReview = asyncHandler(async (req, res, next)=>{ 
+   const review = await Review.findById(req.params.id);
+   if(!review){
+    return next(new ErrorResponse(`No review with the id of ${req.params.id}`, 404));
+   } 
+   if(review.user.toString()!= req.user.id && req.user.role!=='admin'){
+    return next(new ErrorResponse('Not authorize to update review', 401))
+   }
+    await review.remove();  
+  res.status(201).json({
+    success: true,
+    data:  {}
+  })
+})
